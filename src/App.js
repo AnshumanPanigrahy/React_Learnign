9d9c128a8c96da7163c4d7e9d869525215482898
Anshuman_Panigrahy <anshumanpanigrahy579@gmail.com>
2026-06-15 20:03:24 +0530

    useRef DOM  manipulation Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import './App.css';
-import {useState,useRef} from 'react'
+import {useRef,forwardRef} from 'react'
 
+
+const MyBtn=forwardRef(
+    (props,ref)=>{
+    console.log(props)
+    return (
+        <input ref={ref} placeholder='Serching'/>
+    );
+})
 
 
 function App() {
-    const [count, setCount] = useState(0);
-    const timerRef = useRef("Anshuman");
-    // eslint-disable-next-line no-unused-vars
 
-    const onStartClick=()=>{
-            timerRef.current=setInterval(()=>{
-                setCount(count => count + 1)
-            },100)
-        console.log({timerRef})
+    const inputRef = useRef(0);
+
+    const onFocusClick = () => {
+        if(inputRef.current){
+            inputRef.current.focus();
+        }
     }
-     const onStopClick=()=>{
-        clearInterval(timerRef.current)
-         console.log({timerRef})
-     }
+
     return (
     <>
-      <p>Count-{count}</p>
-        <button onClick={onStartClick}>Start</button>
-        <button onClick={onStopClick}>Stop</button>
+        <MyBtn placeholder="Searching Something" ref={inputRef}/>
+        <button onClick={onFocusClick}>Focus on Input</button>
     </>
   );
 }
